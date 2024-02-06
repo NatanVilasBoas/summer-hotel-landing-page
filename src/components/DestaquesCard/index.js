4db9158679d72caa933045a1336fb36ef41c139e
Natan200-2 <natanboas21@outlook.com>
2024-02-06 14:45:06 -0300

    ajustado responsividade de destaques

--- src/components/DestaquesCard/index.js
+++ src/components/DestaquesCard/index.js
@@ -2,7 +2,7 @@ import styles from './DestaquesCard.module.css';
 
 function DestaquesCard(props) {
     return(
-        <div className={`card mb-3 ${styles.card}`}>
+        <div className={`card col-12 ${styles.card}`}>
             <img src={props.src} className={`card-img-top img-fluid`} alt={props.title}/>
             <div className={`card-body ${styles.cardBack}`}>
                 <h5 className={`card-title ${styles.cardTitle}`}>{props.title}</h5>

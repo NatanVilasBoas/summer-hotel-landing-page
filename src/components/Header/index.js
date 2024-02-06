f0d2004a69519f0bfce6703aea34d61bcfa67828
Natan200-2 <natanboas21@outlook.com>
2024-02-06 14:00:34 -0300

    aplicando responsividade em header e content

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,62 @@
+import { useState } from 'react';
 import styles from './Header.module.css';
-import  logo  from './ferias.png';
+import logo from './ferias.png';
+import { useEffect } from 'react';
 
 
 function Header() {
-    return(
+
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 768); // Define o limite de largura para quando o botão do menu deve aparecer
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize(); // Verifica o tamanho da tela ao carregar o componente
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
         <div>
-            <section className={styles.container}>
-                <div>
-                    <a href='#inicio'>Início</a>
-                    <a href='#destaques'>Destaques</a>
-                    <a href='#acomodacoes'>Acomodações</a>
-                    <a href='#equipe'>Equipe</a>
-                    <a href='#contato'>Contato</a>
-                </div>
-                <img src={logo} />
-            </section>
+            {isSmallScreen ?
+                (
+                    <section className={styles.container}>
+                        <div>
+                            <div className="dropdown">
+                                <button className="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M4 6H20M4 12H14M4 18H9" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                    </svg>
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className='dropwdown-item' href='#inicio'>Início</a></li>
+                                    <li><a className='dropwdown-item' href='#destaques'>Destaques</a></li>
+                                    <li><a className='dropwdown-item' href='#acomodacoes'>Acomodações</a></li>
+                                    <li><a className='dropwdown-item' href='#equipe'>Equipe</a></li>
+                                    <li><a className='dropwdown-item' href='#contato'>Contato</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </section>
+                )
+                :
+                (
+                    <section className={styles.container}>
+                        <div>
+                            <a href='#inicio'>Início</a>
+                            <a href='#destaques'>Destaques</a>
+                            <a href='#acomodacoes'>Acomodações</a>
+                            <a href='#equipe'>Equipe</a>
+                            <a href='#contato'>Contato</a>
+                        </div>
+                        <img src={logo} />
+                    </section>
+                )
+            }
+
         </div>
     )
 }

8d074122b595d315a4a73f3106b10e74bfd2e040
Natan200-2 <natanboas21@outlook.com>
2024-02-06 14:29:43 -0300

    ajuste em responsividade em cards

--- src/components/DestaquesCard/index.js
+++ src/components/DestaquesCard/index.js
@@ -2,8 +2,8 @@ import styles from './DestaquesCard.module.css';
 
 function DestaquesCard(props) {
     return(
-        <div className={`card mb-3 d-block w-100 ${styles.card}`}>
-            <img src={props.src} className={`card-img-top ${styles.cardImg}`} alt={props.title}/>
+        <div className={`card mb-3 ${styles.card}`}>
+            <img src={props.src} className={`card-img-top img-fluid`} alt={props.title}/>
             <div className={`card-body ${styles.cardBack}`}>
                 <h5 className={`card-title ${styles.cardTitle}`}>{props.title}</h5>
                 <p className={`card-text ${styles.cardText}`}>{props.text}</p>
